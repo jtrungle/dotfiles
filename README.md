3e52a578a3a6ab12133c1de0b71280fb68b2a157
Johnny Le <johnny.le@droneshield.com>
2026-01-23 17:04:56 +1100

    add nix

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 
 git config --global core.excludesfile ~/.gitignore_global
 ```
+
+# Nix
+
+```
+nix run home-manager switch -- --flake .#johnny
+```
--- nix/home.nix
+++ nix/home.nix
@@ -11,8 +11,36 @@
     ripgrep
     fd
     bat
+    stow
+    zoxide
+    tldr
+    lazydocker
+    lazygit
+    fzf
+    eza
+    uv
+    neovim
+    opencode
+  ];
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = true;
+    
+  };
+  programs.zoxide.options = [
+    "--cmd cd"
   ];
 
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      ls = "eza -l --icons --group-directories-first -a --git";
+      hm = "nix run home-manager switch -- --flake /home/johnny/dotfiles/nix#johnny";
+    };
+    bashrcExtra = ''
+        export PS1="\[$(tput setaf 33)\]\u\[$(tput setaf 69)\]@\[$(tput setaf 105)\]\h \[$(tput setaf 141)\]\w \[$(tput sgr0)\]$ "
+        '' ;
+  };
   # Environment vars only (safe with Stow)
   home.sessionVariables = {
     EDITOR = "nvim";
