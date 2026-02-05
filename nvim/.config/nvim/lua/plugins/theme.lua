b21486499876584c829f3902d98a5bb30c87bf61
Johnny Le <johnnytrungle@gmail.com>
2026-02-06 00:27:36 +1100

    updates

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,12 +1,38 @@
 return {
   -- add gruvbox
   { "samharju/synthweave.nvim" },
+  {
+    "scottmckendry/cyberdream.nvim",
 
+    config = function()
+      require("cyberdream").setup({
+        transparent=true
+      })
+    end
+  },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "synthweave",
+      -- colorscheme = "cyberdream",
     },
   },
+  {
+    "zaldih/themery.nvim",
+    lazy = false,
+    config = function()
+      require("themery").setup({
+        themes = { {
+          name = "cyberdream",
+          colorscheme = "cyberdream",
+        },
+          {
+            name = "synthweave",
+            colorscheme = "synthweave",
+          } },
+        -- add the config here
+      })
+    end
+  }
 }
