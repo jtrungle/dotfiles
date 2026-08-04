8ed8e1efaea8b6f24395d6849a71759607d32efb
Johnny Le <johnnytrungle@gmail.com>
2026-08-04 20:16:50 +1000

    go back to pyright

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -3,10 +3,7 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        ty = {
-          enabled = true
-        }, -- 👈 enable Astral ty LSP
-        pyright = false
+        pyright = true
       },
     },
   },
